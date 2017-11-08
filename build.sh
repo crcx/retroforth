5aa2f45b41b5381ef0b26e2f4e3d1d44a2c9e02c
crc <crc@noemail.net>
2017-11-08 15:43:52 +0000

    add a build.sh script
    FossilOrigin-Name: 582eae019d3f9ec71f7695f9eac7a6e95a287eb3ea3b252fe4cf78fce12a0a08

---
+++ build.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+rm -f bin/rre
+rm -f bin/embedimage
+rm -f bin/repl
+rm -f bin/extend
+rm -f bin/muri
+
+cd tools
+cc -O3 -c embedimage.c -o embedimage.o
+cc -O3 -c extend.c -o extend.o
+cc -O3 -c unu.c -o unu.o
+cc -O3 -c muri.c -o muri.o
+cc unu.o        -lm -o unu
+cc muri.o       -lm -o muri
+cc embedimage.o -lm -o embedimage
+cc extend.o     -lm -o extend
+mv embedimage ../bin
+mv extend ../bin
+mv unu ../bin
+mv muri ../bin
+rm *.o
+cd ..
+
+./bin/unu literate/Unu.md >tools/unu.c
+./bin/unu literate/Muri.md >tools/muri.c
+./bin/muri literate/Rx.md
+./bin/extend literate/RetroForth.md
+
+cp ngaImage interfaces
+cd interfaces
+../bin/extend rre.forth
+../bin/embedimage >image.c
+rm ngaImage
+cc -O3 -c rre.c -o rre.o
+cc -O3 -c repl.c -o repl.o
+cc rre.o -lm -o rre
+cc repl.o -lm -o repl
+mv rre ../bin
+mv repl ../bin
+rm *.o
+cd ..
