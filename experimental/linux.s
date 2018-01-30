a49e02ea3fd82b4995369bbdfe369203885f6681
crc <crc@noemail.net>
2018-01-30 21:30:49 +0000

    experimental: add video driver code from Sam Falvos FTS/Forth [was previously included as part of RETRO9, source/video.asm and source/util.asm]
    FossilOrigin-Name: e19d6ecfb86ebc92711b67aefede96ff3c95126797243c2565b64cd3312b3255

--- experimental/linux.s
+++ experimental/linux.s
@@ -1,4 +1,14 @@
+;  ___ ___ _____ ___  ___      __  _    _
+; | _ \ __|_   _| _ \/ _ \    / / | |  (_)_ _ _  ___ __
+; |   / _|  | | |   / (_) |  / /  | |__| | ' \ || \ \ /
+; |_|_\___| |_| |_|_\\___/  /_/   |____|_|_||_\_,_/_\_\
+;
+; This is the minimal startup + I/O functionality needed to run
+; RETRO on a Linux x86 system.
+; =============================================================
+
 bits 32
+
 section .text
 global putchar
 global getchar
@@ -6,35 +16,36 @@ global _start
 extern main
 
 align 4
+
 _start:
-	call main
-	jmp $
+  call main
+  jmp  $
 
 align 4
 putchar:
-	mov eax, [esp+4]
-	mov [buf], eax
-	mov edx, 1
-	mov ecx, buf
-	mov ebx, 1
-	mov eax, 4
-	int 0x80
-	ret
+  mov eax, [esp+4]
+  mov [buf], eax
+  mov edx, 1
+  mov ecx, buf
+  mov ebx, 1
+  mov eax, 4
+  int 0x80
+  ret
 
 align 4
 getchar:
-	mov edx, 1
-	mov ecx, buf
-	mov ebx, 0
-	mov eax, 3
-	int 0x80
-	mov eax, 0
-	mov eax, [buf]
-	ret
+  mov edx, 1
+  mov ecx, buf
+  mov ebx, 0
+  mov eax, 3
+  int 0x80
+  mov eax, 0
+  mov eax, [buf]
+  ret
 
 section .data
 buf:
-	dd 0
-	dd 0
-	dd 0
-	dd 0
+  dd 0
+  dd 0
+  dd 0
+  dd 0
