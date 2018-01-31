52dc00690f8d0447d38dd2406677b429ac5ca40e
crc <crc@noemail.net>
2018-01-31 17:03:21 +0000

    experimental: cleanup linux assembly formatting
    FossilOrigin-Name: 5f7a190d7c1c18a671c1c978ad2402912d618976566d159b7fb712db8a8d4f30

--- experimental/linux.s
+++ experimental/linux.s
@@ -18,34 +18,34 @@ extern main
 align 4
 
 _start:
-  call main
-  jmp  $
+        call    main
+        jmp     $
 
 align 4
 putchar:
-  mov eax, [esp+4]
-  mov [buf], eax
-  mov edx, 1
-  mov ecx, buf
-  mov ebx, 1
-  mov eax, 4
-  int 0x80
-  ret
+        mov     eax, [esp+4]
+        mov     [buf], eax
+        mov     edx, 1
+        mov     ecx, buf
+        mov     ebx, 1
+        mov     eax, 4
+        int     0x80
+        ret
 
 align 4
 getchar:
-  mov edx, 1
-  mov ecx, buf
-  mov ebx, 0
-  mov eax, 3
-  int 0x80
-  mov eax, 0
-  mov eax, [buf]
-  ret
+        mov     edx, 1
+        mov     ecx, buf
+        mov     ebx, 0
+        mov     eax, 3
+        int     0x80
+        mov     eax, 0
+        mov     eax, [buf]
+        ret
 
 section .data
 buf:
-  dd 0
-  dd 0
-  dd 0
-  dd 0
+        dd 0
+        dd 0
+        dd 0
+        dd 0
