f0d8099acc1f61484c2ece45ad5832f6f6942062
crc <crc@noemail.net>
2018-11-14 22:56:30 +0000

    fix other build files
    FossilOrigin-Name: b5fe54b68d92b98ebf564737cdd65605c429cd0895e06bc55d91ada767c5a95d

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -27,6 +27,7 @@ easier to write. Contrast:
 ## Build
 
 - add `Makefile.linux` to help with building on some Linux systems
+- recreate `bin` if it's not present
 
 ## Core Language
 
@@ -42,6 +43,7 @@ easier to write. Contrast:
 
 - moved Windows implementation to `interfaces/windows`
 - now starts the listener when run w/o any arguments
+- added `-s` command line parameter to silence input echo & ok prompt
 
 ## Tools
 
--- build.sh
+++ build.sh
@@ -1,49 +1,51 @@
 #!/bin/sh
 
+mkdir -p bin
+
 echo Remove Old Binaries
-rm -f bin/unu
-rm -f bin/rre
-rm -f bin/embedimage
-rm -f bin/repl
-rm -f bin/extend
-rm -f bin/muri
+rm -f bin/retro-unu
+rm -f bin/retro
+rm -f bin/retro-embedimage
+rm -f bin/retro-repl
+rm -f bin/retro-extend
+rm -f bin/retro-muri
 
 echo Build Unu
-cc -O2 tools/unu.c -o bin/unu
+cc -O2 tools/unu.c -o bin/retro-unu
 
 echo Updating Sources
-./bin/unu literate/Unu.md >tools/unu.c
-./bin/unu literate/Muri.md >tools/muri.c
+./bin/retro-unu literate/Unu.md >tools/unu.c
+./bin/retro-unu literate/Muri.md >tools/muri.c
 
 echo Build Toolchain
-cc -O2 tools/embedimage.c -o bin/embedimage
-cc -O2 tools/extend.c -o bin/extend
-cc -O2 tools/muri.c -o bin/muri
+cc -O2 tools/embedimage.c -o bin/retro-embedimage
+cc -O2 tools/extend.c -o bin/retro-extend
+cc -O2 tools/muri.c -o bin/retro-muri
 
 echo Assemble ngaImage
-./bin/muri literate/Rx.md
-./bin/extend literate/RetroForth.md
+./bin/retro-muri literate/Rx.md
+./bin/retro-extend literate/RetroForth.md
 
 echo Prepare Image for Embedding [repl, ri]
-./bin/embedimage > interfaces/image.c
+./bin/retro-embedimage > interfaces/image.c
 
-echo Prepare Extended Image for Embedding [rre/unix]
+echo Prepare Extended Image for Embedding [bin/retro-/unix]
 cp ngaImage cleanImage
-./bin/extend interfaces/rre.forth
-./bin/embedimage >interfaces/rre_image_unix.c
+./bin/retro-extend interfaces/rre.forth
+./bin/retro-embedimage >interfaces/rre_image_unix.c
 mv cleanImage ngaImage
 
 echo Compile repl
 cd interfaces
-cc -O2 repl.c -o ../bin/repl
+cc -O2 repl.c -o ../bin/retro-repl
 cd ..
 
 echo Compile rre/unix
 cd interfaces
-cc -O2 rre.c -lm -o ../bin/rre
+cc -O2 rre.c -lm -o ../bin/retro
 cd ..
 
 echo "Update Glossary"
 LC_ALL=c sort -o sorted.tsv words.tsv
 mv sorted.tsv words.tsv
-./bin/rre glossary.forth export glossary >doc/Glossary.txt
+./bin/retro glossary.forth export glossary >doc/Glossary.txt
