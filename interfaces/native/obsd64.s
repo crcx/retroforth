9398560f739522ab51d061a7d09bb5863b6d480f
crc <crc@noemail.net>
2019-01-14 03:26:25 +0000

    native: now builds native386 under openbsd, start work on openbsd/amd64 support
    FossilOrigin-Name: 90f1a6751eb4b7caa80df98f1995dbd70890e150d29f314e978a88072732b133

---
+++ interfaces/native/obsd64.s
@@ -0,0 +1,44 @@
+; This is the minimal startup + I/O functionality needed to run
+; RETRO on an OpenBSD x86-64 system.
+; =============================================================
+
+bits 64
+section .text
+global putchar
+global getchar
+global _start
+extern main
+
+align 8
+_start:
+        call    main
+        jmp     $
+
+align 8
+putchar:
+        mov     rax, rdi
+        mov     [buf], eax
+        mov     rax, 4        ; sys_write
+        mov     rdi, 1        ; stdout
+        mov     rsi, buf      ; address
+        mov     rdx, 1        ; 1 byte
+        syscall
+        ret
+
+align 8
+getchar:
+        mov     rax, 3        ; sys_read
+        mov     rdi, 0        ; stdin
+        mov     rsi, buf      ; address
+        mov     rdx, 1        ; 1 byte
+        syscall
+        mov     rax, 0
+        mov     eax, [buf]
+        ret
+
+section .data
+buf:
+        dd 0
+        dd 0
+        dd 0
+        dd 0
