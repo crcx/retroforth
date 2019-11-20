ec741aca4f4b7dc99175fbdeed8624e68c8ed663
crc <crc@noemail.net>
2019-11-20 17:06:57 +0000

    nga-js: reformat nga.js
    FossilOrigin-Name: 67b34a2ff86570475143b8e8e9981aef79048b1a9920fde4994888585e146d18

--- nga-js/nga.js
+++ nga-js/nga.js
@@ -6,82 +6,82 @@
    Copyright (c) 2011,        Kenneth Keating
    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
-var IMAGE_SIZE = 524288 * 4; /* Amount of simulated RAM    */
-var DATA_DEPTH = 8192; /* Depth of data stack        */
-var ADDRESS_DEPTH = 32768; /* Depth of the stacks        */
+var IMAGE_SIZE = 524288;    /* Amount of simulated RAM    */
+var DATA_DEPTH = 8192;      /* Depth of data stack        */
+var ADDRESS_DEPTH = 32768;  /* Depth of the stacks        */
 
 function Stack(size) {
+  this.sp = 0;
+  this.data = new Array(size);
+  this.push = function(n) {
+    this.sp++;
+    this.data[this.sp] = n;
+  }
+  this.pop = function() {
+    return this.data[this.sp--];
+  }
+  this.depth = function() {
+    return this.sp;
+  }
+  this.tos = function() {
+    return this.data[this.sp];
+  }
+  this.nos = function() {
+    return this.data[this.sp - 1];
+  }
+  this.dup = function() {
+    this.push(this.tos());
+  }
+  this.drop = function() {
+    this.sp--;
+  }
+  this.swap = function() {
+    var a = this.nos();
+    this.data[this.sp - 1] = this.tos();
+    this.data[this.sp] = a;
+  }
+  this.inc = function() {
+    this.data[this.sp]++;
+  }
+  this.dec = function() {
+    this.data[this.sp]--;
+  }
+  this.reset = function() {
     this.sp = 0;
-    this.data = new Array(size);
-    this.push = function(n) {
-        this.sp++;
-        this.data[this.sp] = n;
-    }
-    this.pop = function() {
-        return this.data[this.sp--];
-    }
-    this.depth = function() {
-        return this.sp;
-    }
-    this.tos = function() {
-        return this.data[this.sp];
-    }
-    this.nos = function() {
-        return this.data[this.sp - 1];
-    }
-    this.dup = function() {
-        this.push(this.tos());
-    }
-    this.drop = function() {
-        this.sp--;
-    }
-    this.swap = function() {
-        var a = this.nos();
-        this.data[this.sp - 1] = this.tos();
-        this.data[this.sp] = a;
-    }
-    this.inc = function() {
-        this.data[this.sp]++;
-    }
-    this.dec = function() {
-        this.data[this.sp]--;
-    }
-    this.reset = function() {
-        this.sp = 0;
-    }
+  }
 }
 
 function Opcodes() {
-    this.NOP = 0;
-    this.LIT = 1;
-    this.DUP = 2;
-    this.DROP = 3;
-    this.SWAP = 4;
-    this.PUSH = 5;
-    this.POP = 6;
-    this.JUMP = 7;
-    this.CALL = 8;
-    this.CCALL = 9;
-    this.RETURN = 10;
-    this.EQ = 11;
-    this.NEQ = 12;
-    this.LT = 13;
-    this.GT = 14;
-    this.FETCH = 15;
-    this.STORE = 16;
-    this.ADD = 17;
-    this.SUB = 18;
-    this.MUL = 19;
-    this.DIVMOD = 20;
-    this.AND = 21;
-    this.OR = 22;
-    this.XOR = 23;
-    this.SHIFT = 24;
-    this.ZERO_EXIT = 25;
-    this.END = 26;
-    this.IE = 27;
-    this.IQ = 28;
-    this.II = 29;
+  this.NOP = 0;
+  this.LIT = 1;
+  this.DUP = 2;
+  this.DROP = 3;
+  this.SWAP = 4;
+  this.PUSH = 5;
+  this.POP = 6;
+  this.JUMP = 7;
+  this.CALL = 8;
+  this.CCALL = 9;
+  this.RETURN = 10;
+  this.EQ = 11;
+  this.NEQ = 12;
+  this.LT = 13;
+  this.GT = 14;
+  this.FETCH = 15;
+  this.STORE = 16;
+  this.ADD = 17;
+  this.SUB = 18;
+  this.MUL = 19;
+  this.DIVMOD = 20;
+  this.AND = 21;
+  this.OR = 22;
+  this.XOR = 23;
+  this.SHIFT = 24;
+  this.ZERO_EXIT = 25;
+  this.END = 26;
+  this.IE = 27;
+  this.IQ = 28;
+  this.II = 29;
 }
 var ip = 0;
 var data = new Stack(DATA_DEPTH);
@@ -91,407 +91,407 @@ var vm = new Opcodes();
 var instructions = new Array(vm.II + 1);
 
 function rxPrepareVM() {
-    ip = 0;
-    data.reset();
-    address.reset();
+  ip = 0;
+  data.reset();
+  address.reset();
 }
 
 instructions[vm.NOP] = function() {}
 instructions[vm.LIT] = function() {
-    ip++;
-    data.push(image[ip]);
+  ip++;
+  data.push(image[ip]);
 }
 instructions[vm.DUP] = function() {
-    data.dup();
+  data.dup();
 }
 instructions[vm.DROP] = function() {
-    data.drop();
+  data.drop();
 }
 instructions[vm.SWAP] = function() {
-    data.swap();
+  data.swap();
 }
 instructions[vm.PUSH] = function() {
-    address.push(data.pop());
+  address.push(data.pop());
 }
 instructions[vm.POP] = function() {
-    data.push(address.pop())
+  data.push(address.pop())
 }
 instructions[vm.JUMP] = function() {
-    ip = data.pop() - 1;
+  ip = data.pop() - 1;
 }
 instructions[vm.CALL] = function() {
-    address.push(ip);
-    ip = data.pop() - 1;
+  address.push(ip);
+  ip = data.pop() - 1;
 }
 instructions[vm.CCALL] = function() {
-    var a, b;
-    a = data.pop();
-    b = data.pop();
-    if (b != 0) {
-        address.push(ip);
-        ip = a - 1;
-    }
+  var a, b;
+  a = data.pop();
+  b = data.pop();
+  if (b != 0) {
+    address.push(ip);
+    ip = a - 1;
+  }
 }
 instructions[vm.RETURN] = function() {
-    ip = address.pop();
+  ip = address.pop();
 }
 instructions[vm.EQ] = function() {
-    var a, b;
-    a = data.pop();
-    b = data.pop();
-    if (b == a)
-        data.push(-1);
-    else
-        data.push(0);
+  var a, b;
+  a = data.pop();
+  b = data.pop();
+  if (b == a)
+    data.push(-1);
+  else
+    data.push(0);
 }
 instructions[vm.NEQ] = function() {
-    var a, b;
-    a = data.pop();
-    b = data.pop();
-    if (b != a)
-        data.push(-1);
-    else
-        data.push(0);
+  var a, b;
+  a = data.pop();
+  b = data.pop();
+  if (b != a)
+    data.push(-1);
+  else
+    data.push(0);
 }
 instructions[vm.LT] = function() {
-    var a, b;
-    a = data.pop();
-    b = data.pop();
-    if (b < a)
-        data.push(-1);
-    else
-        data.push(0);
+  var a, b;
+  a = data.pop();
+  b = data.pop();
+  if (b < a)
+    data.push(-1);
+  else
+    data.push(0);
 }
 instructions[vm.GT] = function() {
-    var a, b;
-    a = data.pop();
-    b = data.pop();
-    if (b > a)
-        data.push(-1);
-    else
-        data.push(0);
+  var a, b;
+  a = data.pop();
+  b = data.pop();
+  if (b > a)
+    data.push(-1);
+  else
+    data.push(0);
 }
 instructions[vm.FETCH] = function() {
-    x = data.pop();
-    if (x == -1)
-        data.push(data.sp);
-    else if (x == -2)
-        data.push(address.sp);
-    else if (x == -3)
-        data.push(IMAGE_SIZE);
-    else if (x == -4)
-        data.push(-2147483648);
-    else if (x == -5)
-        data.push(2147483647);
-    else
-        data.push(image[x]);
+  x = data.pop();
+  if (x == -1)
+    data.push(data.sp);
+  else if (x == -2)
+    data.push(address.sp);
+  else if (x == -3)
+    data.push(IMAGE_SIZE);
+  else if (x == -4)
+    data.push(-2147483648);
+  else if (x == -5)
+    data.push(2147483647);
+  else
+    data.push(image[x]);
 }
 instructions[vm.STORE] = function() {
-    image[data.tos()] = data.nos();
-    data.drop();
-    data.drop();
+  image[data.tos()] = data.nos();
+  data.drop();
+  data.drop();
 }
 instructions[vm.ADD] = function() {
-    var x = data.pop();
-    var y = data.pop();
-    data.push(x + y);
+  var x = data.pop();
+  var y = data.pop();
+  data.push(x + y);
 }
 instructions[vm.SUB] = function() {
-    var x = data.pop();
-    var y = data.pop();
-    data.push(y - x);
+  var x = data.pop();
+  var y = data.pop();
+  data.push(y - x);
 }
 instructions[vm.MUL] = function() {
-    var x = data.pop();
-    var y = data.pop();
-    data.push(y * x);
+  var x = data.pop();
+  var y = data.pop();
+  data.push(y * x);
 }
 instructions[vm.DIVMOD] = function() {
-    var b = data.pop();
-    var a = data.pop();
-    if (b == 0) {
-        ip = 0;
-        data.sp = 0;
-        address.sp = 0;
-    } else {
-        var x = Math.abs(b);
-        var y = Math.abs(a);
-        var q = Math.floor(y / x);
-        var r = y % x;
-        if (a < 0 && b < 0)
-            r = r * -1;
-        if (a > 0 && b < 0)
-            q = q * -1;
-        if (a < 0 && b > 0) {
-            r = r * -1;
-            q = q * -1;
-        }
-        data.push(r);
-        data.push(q);
+  var b = data.pop();
+  var a = data.pop();
+  if (b == 0) {
+    ip = 0;
+    data.sp = 0;
+    address.sp = 0;
+  } else {
+    var x = Math.abs(b);
+    var y = Math.abs(a);
+    var q = Math.floor(y / x);
+    var r = y % x;
+    if (a < 0 && b < 0)
+      r = r * -1;
+    if (a > 0 && b < 0)
+      q = q * -1;
+    if (a < 0 && b > 0) {
+      r = r * -1;
+      q = q * -1;
     }
+    data.push(r);
+    data.push(q);
+  }
 }
 instructions[vm.AND] = function() {
-    var x = data.pop();
-    var y = data.pop();
-    data.push(x & y);
+  var x = data.pop();
+  var y = data.pop();
+  data.push(x & y);
 }
 instructions[vm.OR] = function() {
-    var x = data.pop();
-    var y = data.pop();
-    data.push(x | y);
+  var x = data.pop();
+  var y = data.pop();
+  data.push(x | y);
 }
 instructions[vm.XOR] = function() {
-    var x = data.pop();
-    var y = data.pop();
-    data.push(x ^ y);
+  var x = data.pop();
+  var y = data.pop();
+  data.push(x ^ y);
 }
 instructions[vm.SHIFT] = function() {
-    var x = data.pop();
-    var y = data.pop();
-    if (x < 0)
-        data.push(y << (x * -1));
-    else
-        data.push(y >>= x);
+  var x = data.pop();
+  var y = data.pop();
+  if (x < 0)
+    data.push(y << (x * -1));
+  else
+    data.push(y >>= x);
 }
 instructions[vm.ZERO_EXIT] = function() {
-    if (data.tos() == 0) {
-        data.drop();
-        ip = address.pop();
-    }
+  if (data.tos() == 0) {
+    data.drop();
+    ip = address.pop();
+  }
 }
 instructions[vm.END] = function() {
-    ip = IMAGE_SIZE;
+  ip = IMAGE_SIZE;
 }
 instructions[vm.IE] = function() {
-    data.push(1);
+  data.push(1);
 }
 instructions[vm.IQ] = function() {
-    data.drop();
-    data.push(0);
-    data.push(0);
+  data.drop();
+  data.push(0);
+  data.push(0);
 }
 instructions[vm.II] = function() {
-    data.pop();
-    var s = String.fromCharCode(data.pop());
-    document.getElementById('console').innerHTML += s;
+  data.pop();
+  var s = String.fromCharCode(data.pop());
+  document.getElementById('console').innerHTML += s;
 }
 window.addEventListener('load', function(e) {
-    rxPrepareVM();
+  rxPrepareVM();
 }, false);
 
 function processOpcode(opcode) {
-    if (opcode != 0) {
-        instructions[opcode]();
-        checkStack();
-    }
+  if (opcode != 0) {
+    instructions[opcode]();
+    checkStack();
+  }
 }
 
 function validatePackedOpcodes(opcode) {
-    var raw = opcode;
-    var current;
-    var valid = -1;
-    var i = 0;
-    while (i < 4) {
-        current = raw & 0xFF;
-        if (!current >= 0 && current <= 29)
-            valid = 0;
-        raw = raw >> 8;
-        i++;
-    }
+  var raw = opcode;
+  var current;
+  var valid = -1;
+  var i = 0;
+  while (i < 4) {
+    current = raw & 0xFF;
+    if (!current >= 0 && current <= 29)
+      valid = 0;
+    raw = raw >> 8;
+    i++;
+  }
 }
 
 function ngaProcessPackedOpcodes(opcode) {
-    var raw = opcode;
-    ops = new Array(3);
-    ops[0] = raw & (255);
-    raw = raw >> 8;
-    ops[1] = raw & (255);
-    raw = raw >> 8;
-    ops[2] = raw & (255);
-    raw = raw >> 8;
-    ops[3] = raw & (255);
-    processOpcode(ops[0]);
-    processOpcode(ops[1]);
-    processOpcode(ops[2]);
-    processOpcode(ops[3]);
+  var raw = opcode;
+  ops = new Array(3);
+  ops[0] = raw & (255);
+  raw = raw >> 8;
+  ops[1] = raw & (255);
+  raw = raw >> 8;
+  ops[2] = raw & (255);
+  raw = raw >> 8;
+  ops[3] = raw & (255);
+  processOpcode(ops[0]);
+  processOpcode(ops[1]);
+  processOpcode(ops[2]);
+  processOpcode(ops[3]);
 }
 
 
 function loadInitialImage() {
-    image = ngaImage.slice();
+  image = ngaImage.slice();
 }
 
 function execute(offset) {
-    var opcode;
-    address.sp = 1;
-    ip = offset;
-    var notfound = d_xt_for("err:notfound");
-    while (ip < IMAGE_SIZE) {
-        opcode = image[ip];
-        if (ip == notfound) {
-            document.getElementById('console').value +=
-                "err:notfound : " + string_extract(1471) + "\n";
-        }
-        if (validatePackedOpcodes(opcode) != 0) {
-            ngaProcessPackedOpcodes(opcode);
-        } else {
-            alert("Invalid instruction!");
-            alert("At " + ip + ", opcode " + opcode);
-        }
-        if (address.sp == 0)
-            ip = IMAGE_SIZE;
-        ip++;
+  var opcode;
+  address.sp = 1;
+  ip = offset;
+  var notfound = d_xt_for("err:notfound");
+  while (ip < IMAGE_SIZE) {
+    opcode = image[ip];
+    if (ip == notfound) {
+      document.getElementById('console').value +=
+        "err:notfound : " + string_extract(1471) + "\n";
     }
+    if (validatePackedOpcodes(opcode) != 0) {
+      ngaProcessPackedOpcodes(opcode);
+    } else {
+      alert("Invalid instruction!");
+      alert("At " + ip + ", opcode " + opcode);
+    }
+    if (address.sp == 0)
+      ip = IMAGE_SIZE;
+    ip++;
+  }
 }
 
 function checkStack() {
-    var depth = data.depth();
-    var adepth = address.depth();
-    var flag = 0;
-    if (depth < 0 || adepth < 0) {
-        flag = -1;
-    }
-    if (depth > DATA_DEPTH || adepth > DATA_DEPTH) {
-        flag = -1;
-    }
-    if (flag == -1) {
-        ip = 0;
-        data.sp = 0;
-        address.sp = 0;
-    }
+  var depth = data.depth();
+  var adepth = address.depth();
+  var flag = 0;
+  if (depth < 0 || adepth < 0) {
+    flag = -1;
+  }
+  if (depth > DATA_DEPTH || adepth > DATA_DEPTH) {
+    flag = -1;
+  }
+  if (flag == -1) {
+    ip = 0;
+    data.sp = 0;
+    address.sp = 0;
+  }
 }
 
 function string_inject(str, buffer) {
-    var m = str.length;
-    var i = 0;
-    while (m > 0) {
-        image[buffer + i] = str[i].charCodeAt(0);
-        image[buffer + i + 1] = 0;
-        m--;
-        i++;
-    }
+  var m = str.length;
+  var i = 0;
+  while (m > 0) {
+    image[buffer + i] = str[i].charCodeAt(0);
+    image[buffer + i + 1] = 0;
+    m--;
+    i++;
+  }
 }
 
 function string_extract(at) {
-    string_data = "";
-    var starting = at;
-    var i = 0;
-    while (image[starting] != 0)
-        string_data += String.fromCharCode(image[starting++]);
-    return string_data;
+  string_data = "";
+  var starting = at;
+  var i = 0;
+  while (image[starting] != 0)
+    string_data += String.fromCharCode(image[starting++]);
+  return string_data;
 }
 
 function d_link(dt) {
-    return dt + 0;
+  return dt + 0;
 }
 
 function d_xt(dt) {
-    return dt + 1;
+  return dt + 1;
 }
 
 function d_class(dt) {
-    return dt + 2;
+  return dt + 2;
 }
 
 function d_name(dt) {
-    return dt + 3;
+  return dt + 3;
 }
 
 function d_count_entries() {
-    var c = 0;
-    var i = image[2];
-    while (image[i] != 0) {
-        c++;
-        i = image[i];
-    }
-    return c;
+  var c = 0;
+  var i = image[2];
+  while (image[i] != 0) {
+    c++;
+    i = image[i];
+  }
+  return c;
 }
 
 function d_lookup(name) {
-    var dt = 0;
-    var i = image[2];
-    var dname;
-    while (image[i] != 0 && i != 0) {
-        dname = string_extract(d_name(i));
-        if (dname == name) {
-            dt = i;
-            i = 0;
-        } else {
-            i = image[i];
-        }
+  var dt = 0;
+  var i = image[2];
+  var dname;
+  while (image[i] != 0 && i != 0) {
+    dname = string_extract(d_name(i));
+    if (dname == name) {
+      dt = i;
+      i = 0;
+    } else {
+      i = image[i];
     }
-    return dt;
+  }
+  return dt;
 }
 
 function d_xt_for(name) {
-    return image[d_xt(d_lookup(name))];
+  return image[d_xt(d_lookup(name))];
 }
 
 function d_class_for(name) {
-    return image[d_class(d_lookup(name))];
+  return image[d_class(d_lookup(name))];
 }
 
 function evaluate(s) {
-    if (s.length == 0)
-        return;
-    var i = d_xt_for("interpret");
-    string_inject(s, 1471);
-    data.push(1471);
-    execute(i);
+  if (s.length == 0)
+    return;
+  var i = d_xt_for("interpret");
+  string_inject(s, 1471);
+  data.push(1471);
+  execute(i);
 }
 
 function cls() {
-    document.getElementById('console').innerHTML = "";
+  document.getElementById('console').innerHTML = "";
 }
 
 function unu(src) {
-    raw = src.split("\n");
-    var i = raw.length;
-    var lines = new Array();
-    var j = 0;
-    var code = 0;
-    while (j < i) {
-        if (code == 1 && raw[j] == "~~~") {
-            code = 0;
-        } else if (code == 0 && raw[j] == "~~~") {
-            code = 1;
-        } else if (code == 1) {
-            lines.push(raw[j]);
-        }
-        j++;
+  raw = src.split("\n");
+  var i = raw.length;
+  var lines = new Array();
+  var j = 0;
+  var code = 0;
+  while (j < i) {
+    if (code == 1 && raw[j] == "~~~") {
+      code = 0;
+    } else if (code == 0 && raw[j] == "~~~") {
+      code = 1;
+    } else if (code == 1) {
+      lines.push(raw[j]);
     }
-    return lines.join(" ");
+    j++;
+  }
+  return lines.join(" ");
 }
 
 function go() {
-    rxPrepareVM();
-    loadInitialImage();
-    document.getElementById("console").innerHTML = "";
-    src = document.getElementById("input").value;
-    tokens = unu(src).match(/\S+/g);
-    var i = tokens.length;
-    var j = 0;
-    while (j < i) {
-        evaluate(tokens[j]);
-        j++;
-    }
-    s = "";
-    i = data.depth();
-    j = 1;
-    while (j <= i) {
-        s = s + data.data[j] + " ";
-        j++;
-    }
-    document.getElementById("console").innerHTML += "\n" + s;
+  rxPrepareVM();
+  loadInitialImage();
+  document.getElementById("console").innerHTML = "";
+  src = document.getElementById("input").value;
+  tokens = unu(src).match(/\S+/g);
+  var i = tokens.length;
+  var j = 0;
+  while (j < i) {
+    evaluate(tokens[j]);
+    j++;
+  }
+  s = "";
+  i = data.depth();
+  j = 1;
+  while (j <= i) {
+    s = s + data.data[j] + " ";
+    j++;
+  }
+  document.getElementById("console").innerHTML += "\n" + s;
 }
 
 function saveproject() {
-    src = document.getElementById("input").value;
-    console.log('saving ' + src);
-    localStorage.setItem("Snapshot", src);
+  src = document.getElementById("input").value;
+  console.log('saving ' + src);
+  localStorage.setItem("Snapshot", src);
 }
 
 function loadproject() {
-    src = localStorage.getItem("Snapshot");
-    document.getElementById("input").value = src;
+  src = localStorage.getItem("Snapshot");
+  document.getElementById("input").value = src;
 }
