507f191b76aad87aebcb27fec936a6f87f02ebd5
crc <crc@noemail.net>
2019-12-16 20:09:07 +0000

    retro-document, retro-describe: more work on performance
    FossilOrigin-Name: 4e2c988208fc3c1c09932290e7e8c1407d9045856ee86ecbee296924988be189

--- tools/document.sh
+++ tools/document.sh
@@ -14,8 +14,8 @@
 #
 # ./describe sourcefile
 
-# Extract    | Split       | Strip Blanks            | Sort | Filter | Terminate    | Generate                               | Strip Blanks
-retro-unu $1 | tr " " "\n" | sed '/^[[:space:]]*$/d' | sort | uniq   | tr "\n" "\0" | xargs -0 -n1 retro-describe | cat -s
+# Extract    | Split       | Strip Blanks            | Sort | Filter | Terminate     | Generate                          | Strip Blanks
+retro-unu $1 | tr " " "\n" | sed '/^[[:space:]]*$/d' | sort | uniq   |  tr "\n" "\0" | xargs -0 -n15 -J % retro-describe | cat -s
 
-# Extract    | Split       | Strip Blanks            | Get first | Sort | Filter | Find Prefixes                   | Generate word names | Terminate    | Generate                               | Strip Blanks
-retro-unu $1 | tr " " "\n" | sed '/^[[:space:]]*$/d' | cut -c1-1 | sort | uniq   | grep "[\.\"\@\!\$\`:&'#$.\(\|]" | sed 's/^/prefix:/'  | tr "\n" "\0" | xargs -0 -n1 retro-describe | cat -s
+# Extract    | Split       | Strip Blanks            | Get first | Sort | Filter | Find Prefixes                   | Generate word names | Terminate    | Generate                          | Strip Blanks
+retro-unu $1 | tr " " "\n" | sed '/^[[:space:]]*$/d' | cut -c1-1 | sort | uniq   | grep "[\.\"\@\!\$\`:&'#$.\(\|]" | sed 's/^/prefix:/'  | tr "\n" "\0" | xargs -0 -n15 -J % retro-describe | cat -s
