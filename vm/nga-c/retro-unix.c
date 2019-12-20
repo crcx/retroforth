a2bc939b250d942a0c47bf6cb32f97eb4e917727
crc <crc@noemail.net>
2019-12-20 21:29:06 +0000

    retro-unix: fix an issue w/builds requiring c99 on some systems
    FossilOrigin-Name: 2cdea365bfff9a747112a494c7730c27eb671002a791e86ac8cb9b88cbed68a6

--- vm/nga-c/retro-unix.c
+++ vm/nga-c/retro-unix.c
@@ -1556,10 +1556,11 @@ void query_socket() {
 void io_random() {
   int64_t r = 0;
   char buffer[8];
+  int i;
   int fd = open("/dev/urandom", O_RDONLY);
   read(fd, buffer, 8);
   close(fd);
-  for(int i = 0; i < 8; ++i) {
+  for(i = 0; i < 8; ++i) {
     r = r << 8;
     r += ((int64_t)buffer[i] & 0xFF);
   }
