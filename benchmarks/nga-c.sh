e61c0364dfd877ec0940297231f3dfcb90e30a58
crc <crc@noemail.net>
2020-12-18 20:18:13 +0000

    benchmarks: add a simple `choose` benchmark (#26)
    FossilOrigin-Name: a0cac831c406f978982a1a9af3eebe038c04be485e1c72ed834f3538b4def8d5

--- benchmarks/nga-c.sh
+++ benchmarks/nga-c.sh
@@ -2,3 +2,4 @@
 
 time retro times.retro
 time retro push-drop.retro
+time retro choose.retro
--- benchmarks/nga-python.sh
+++ benchmarks/nga-python.sh
@@ -8,9 +8,11 @@ cp ../ngaImage .
 echo Python3
 time python3 retro.py times.retro
 time python3 retro.py push-drop.retro
+time python3 retro.py choose.retro
 
 echo PyPy
 time pypy retro.py times.retro
 time pypy retro.py push-drop.retro
+time pypy retro.py choose.retro
 
 rm -f retro.py ngaImage
