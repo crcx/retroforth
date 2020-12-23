fbf1444c6a356cbe73325acca216f880b797b38a
crc <crc@noemail.net>
2020-12-23 18:42:53 +0000

    retro-compiler: runtime builds & works again
    FossilOrigin-Name: 37e6d4a2ba2821087e24595ef40b5baa5254d0a4a16e66c3f63dac5733cef6df

--- vm/nga-c/retro-runtime.c
+++ vm/nga-c/retro-runtime.c
@@ -202,7 +202,6 @@ void io_scripting_handler() {
 
 /*=====================================================================*/
 
-
 #include "dev-image.c"
 #include "dev-rng.c"
 #include "dev-floatingpoint.c"
@@ -241,8 +240,8 @@ void rre_execute(CELL cell, int silent) {
       token = TOS;
     }
     opcode = memory[ip];
-    if (ngaValidatePackedOpcodes(opcode) != 0) {
-      ngaProcessPackedOpcodes(opcode);
+    if (validate_opcode_bundle(opcode) != 0) {
+      process_opcode_bundle(opcode);
     } else {
       printf("Invalid instruction!\n");
       printf("At %d, opcode %d\n", ip, opcode);
@@ -428,7 +427,7 @@ void help(char *exename) {
 
 void initialize() {
   CELL i;
-  ngaPrepare();
+  prepare_vm();
   for (i = 0; i < ngaImageCells; i++)
     memory[i] = ngaImage[i];
   update_rx();
@@ -458,7 +457,7 @@ int main(int argc, char **argv) {
   sys_argc = argc;                        /* Point the global argc and */
   sys_argv = argv;                        /* argv to the actual ones   */
 
-  ngaPrepare();
+  prepare_vm();
   loadEmbeddedImage(argv[0]);
   update_rx();
   rre_execute(0, 0);
@@ -623,281 +622,7 @@ void update_rx() {
 
 /*=====================================================================*/
 
-
-/*---------------------------------------------------------------------
-  Instruction Processor
-  ---------------------------------------------------------------------*/
-
-enum vm_opcode {
-  VM_NOP,  VM_LIT,    VM_DUP,   VM_DROP,    VM_SWAP,   VM_PUSH,  VM_POP,
-  VM_JUMP, VM_CALL,   VM_CCALL, VM_RETURN,  VM_EQ,     VM_NEQ,   VM_LT,
-  VM_GT,   VM_FETCH,  VM_STORE, VM_ADD,     VM_SUB,    VM_MUL,   VM_DIVMOD,
-  VM_AND,  VM_OR,     VM_XOR,   VM_SHIFT,   VM_ZRET,   VM_HALT,  VM_IE,
-  VM_IQ,   VM_II
-};
-#define NUM_OPS VM_II + 1
-
-CELL ngaLoadImage(char *imageFile) {
-  FILE *fp;
-  CELL imageSize;
-  long fileLen;
-  CELL i;
-  if ((fp = fopen(imageFile, "rb")) != NULL) {
-    /* Determine length (in cells) */
-    fseek(fp, 0, SEEK_END);
-    fileLen = ftell(fp) / sizeof(CELL);
-    if (fileLen > IMAGE_SIZE) {
-      fclose(fp);
-      printf("Image is larger than alloted space!\n");
-      exit(1);
-    }
-    rewind(fp);
-    /* Read the file into memory */
-    imageSize = fread(&memory, sizeof(CELL), fileLen, fp);
-    fclose(fp);
-  }
-  else {
-    for (i = 0; i < ngaImageCells; i++)
-      memory[i] = ngaImage[i];
-    imageSize = i;
-  }
-  return imageSize;
-}
-
-void ngaPrepare() {
-  ip = sp = rp = 0;
-  for (ip = 0; ip < IMAGE_SIZE; ip++)
-    memory[ip] = VM_NOP;
-  for (ip = 0; ip < STACK_DEPTH; ip++)
-    data[ip] = 0;
-  for (ip = 0; ip < ADDRESSES; ip++)
-    address[ip] = 0;
-}
-
-void inst_nop() {
-}
-
-void inst_lit() {
-  sp++;
-  ip++;
-  TOS = memory[ip];
-}
-
-void inst_dup() {
-  sp++;
-  data[sp] = NOS;
-}
-
-void inst_drop() {
-  data[sp] = 0;
-   if (--sp < 0)
-     ip = IMAGE_SIZE;
-}
-
-void inst_swap() {
-  CELL a;
-  a = TOS;
-  TOS = NOS;
-  NOS = a;
-}
-
-void inst_push() {
-  rp++;
-  TORS = TOS;
-  inst_drop();
-}
-
-void inst_pop() {
-  sp++;
-  TOS = TORS;
-  rp--;
-}
-
-void inst_jump() {
-  ip = TOS - 1;
-  inst_drop();
-}
-
-void inst_call() {
-  rp++;
-  TORS = ip;
-  ip = TOS - 1;
-  inst_drop();
-}
-
-void inst_ccall() {
-  CELL a, b;
-  a = TOS; inst_drop();  /* False */
-  b = TOS; inst_drop();  /* Flag  */
-  if (b != 0) {
-    rp++;
-    TORS = ip;
-    ip = a - 1;
-  }
-}
-
-void inst_return() {
-  ip = TORS;
-  rp--;
-}
-
-void inst_eq() {
-  NOS = (NOS == TOS) ? -1 : 0;
-  inst_drop();
-}
-
-void inst_neq() {
-  NOS = (NOS != TOS) ? -1 : 0;
-  inst_drop();
-}
-
-void inst_lt() {
-  NOS = (NOS < TOS) ? -1 : 0;
-  inst_drop();
-}
-
-void inst_gt() {
-  NOS = (NOS > TOS) ? -1 : 0;
-  inst_drop();
-}
-
-void inst_fetch() {
-  switch (TOS) {
-    case -1: TOS = sp - 1; break;
-    case -2: TOS = rp; break;
-    case -3: TOS = IMAGE_SIZE; break;
-    case -4: TOS = CELL_MIN; break;
-    case -5: TOS = CELL_MAX; break;
-    default: TOS = memory[TOS]; break;
-  }
-}
-
-void inst_store() {
-  if (TOS <= IMAGE_SIZE && TOS >= 0) {
-    memory[TOS] = NOS;
-    inst_drop();
-    inst_drop();
-  } else {
-    ip = IMAGE_SIZE;
-  }
-}
-
-void inst_add() {
-  NOS += TOS;
-  inst_drop();
-}
-
-void inst_sub() {
-  NOS -= TOS;
-  inst_drop();
-}
-
-void inst_mul() {
-  NOS *= TOS;
-  inst_drop();
-}
-
-void inst_divmod() {
-  CELL a, b;
-  a = TOS;
-  b = NOS;
-  TOS = b / a;
-  NOS = b % a;
-}
-
-void inst_and() {
-  NOS = TOS & NOS;
-  inst_drop();
-}
-
-void inst_or() {
-  NOS = TOS | NOS;
-  inst_drop();
-}
-
-void inst_xor() {
-  NOS = TOS ^ NOS;
-  inst_drop();
-}
-
-void inst_shift() {
-  CELL y = TOS;
-  CELL x = NOS;
-  if (TOS < 0)
-    NOS = NOS << (TOS * -1);
-  else {
-    if (x < 0 && y > 0)
-      NOS = x >> y | ~(~0U >> y);
-    else
-      NOS = x >> y;
-  }
-  inst_drop();
-}
-
-void inst_zret() {
-  if (TOS == 0) {
-    inst_drop();
-    ip = TORS;
-    rp--;
-  }
-}
-
-void inst_halt() {
-  ip = IMAGE_SIZE;
-}
-
-void inst_ie() {
-  sp++;
-  TOS = NUM_DEVICES;
-}
-
-void inst_iq() {
-  CELL Device = TOS;
-  inst_drop();
-  IO_queryHandlers[Device]();
-}
-
-void inst_ii() {
-  CELL Device = TOS;
-  inst_drop();
-  IO_deviceHandlers[Device]();
-}
-
-Handler instructions[NUM_OPS] = {
-  inst_nop, inst_lit, inst_dup, inst_drop, inst_swap, inst_push, inst_pop,
-  inst_jump, inst_call, inst_ccall, inst_return, inst_eq, inst_neq, inst_lt,
-  inst_gt, inst_fetch, inst_store, inst_add, inst_sub, inst_mul, inst_divmod,
-  inst_and, inst_or, inst_xor, inst_shift, inst_zret, inst_halt, inst_ie,
-  inst_iq, inst_ii
-};
-
-void ngaProcessOpcode(CELL opcode) {
-  if (opcode != 0)
-    instructions[opcode]();
-}
-
-int ngaValidatePackedOpcodes(CELL opcode) {
-  CELL raw = opcode;
-  CELL current;
-  int valid = -1;
-  int i;
-  for (i = 0; i < 4; i++) {
-    current = raw & 0xFF;
-    if (!(current >= 0 && current <= 29))
-      valid = 0;
-    raw = raw >> 8;
-  }
-  return valid;
-}
-
-void ngaProcessPackedOpcodes(CELL opcode) {
-  CELL raw = opcode;
-  int i;
-  for (i = 0; i < 4; i++) {
-    ngaProcessOpcode(raw & 0xFF);
-    raw = raw >> 8;
-  }
-}
+#include "nga.c"
 
 /*=====================================================================*/
 
