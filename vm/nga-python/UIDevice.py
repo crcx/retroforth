9c93e766fc94d210fe8b7930dd0a9779b6b3f874
crc <crc@noemail.net>
2021-02-01 00:45:55 +0000

    nga-python: latest code from Arland for Pythonista UI module support
    FossilOrigin-Name: 027308b773f14441c3c74a15106c1b261cb32e3b3d96facf7ceb14353130852d

--- vm/nga-python/UIDevice.py
+++ vm/nga-python/UIDevice.py
@@ -20,39 +20,80 @@
 import ui
 
 class UI:
-    def __init__(self, rows=10, cols=10):
+    def __init__(self, title, rows=10, cols=10):
+        self.view = ui.View(frame=(0, 0, 300, 300))
+        self.view.name = title
+        self.view.background_color = 'white'
         self.rows = rows
         self.cols = cols
+        self.w = (self.view.frame[2]/cols)
+        self.h = (self.view.frame[3]/rows)
+        self.subviews = dict()
+        self.actions = dict()
 
-    def create_view(self, name, id):
-        pass
+    def create_view(self, name, which, xywh=[0,0,1,1], flex=None, bgColor='FFFFFF', border_width=0):
+        self.subviews[name] = which
+        self.subviews[name].name = name
+        if flex:
+            self.subviews[name].flex = flex
+        else:
+            self.subviews[name].flex = 'WRHLTB'
+        self.subviews[name].background_color=bgColor
+        self.subviews[name].frame = [xywh[0]*self.w,
+                                                                 xywh[1]*self.h,
+                                                                 xywh[2]*self.w,
+                                                                 xywh[3]*self.h]
+        self.subviews[name].border_width = border_width
 
-    def add_view(self, name):
-        pass
+    def add_view(self, view):
+        self.view.add_subview(self.subviews[view])
 
-    def set_size(self, name, h, w):
-        pass
+    def set_size(self, name, w, h):
+        xywh = list(self.subviews[name].frame)
+        xywh[2] = self.w*w
+        xywh[3] = self.h*h
+        self.subviews[name].frame = xywh
 
     def set_coords(self, name, x, y):
-        pass
-
+        xywh = list(self.subviews[name].frame)
+        xywh[0] = self.w*x
+        xywh[1] = self.h*y
+        self.subviews[name].frame = xywh
+    
+    def get_coords(self, name):
+        xywh = list(self.subviews[name].frame)
+        return (xywh[0], xywh[1])
+    
+    def get_size(self, name):
+        xywh = list(self.subviews[name].frame)
+        return (xywh[3], xywh[2])
+  
     def remove_view(self, name):
-        pass
+         self.view.remove_subview(self.subviews[name])
 
     def set_text(self, name, text):
-        pass
+        self.subviews[name].text = text
 
     def set_title(self, name, text):
-        pass
+        self.subviews[name].title = text
 
     def get_text(self, name):
-        pass
+        return self.subviews[name].text
 
     def get_title(self, name):
-        pass
+        return self.subviews[name].title
 
     def handle_action(self, sender):
-        pass
+        handler, inst = self.actions[sender.name]
+        inst.execute(handler)
 
-    def add_action(self, name, handler):
-        pass
+    def add_action(self, name, handler, inst):
+        self.actions[name] = (handler, inst)
+        self.subviews[name].action = self.handle_action
+    
+    def get_action(self, name, handler, inst):
+        return self.actions[name][0]
+    
+    def present(self):
+        self.view.present('sheet')
+		
