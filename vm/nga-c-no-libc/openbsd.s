2b311ceab758c9c47f6d1d1ba1b626920dd40368
crc <crc@noemail.net>
2021-06-17 18:49:53 +0000

    nga-c: non-libc: openbsd-amd64 support, source cleanups
    FossilOrigin-Name: f8712bb8ce359909a61a8d0ff917173da4e35b8586e34b6207e3604ae40f139e

---
+++ vm/nga-c-no-libc/openbsd.s
@@ -0,0 +1,9 @@
+.section ".note.openbsd.ident", "a"
+        .p2align 2
+        .long   0x8
+        .long   0x4
+        .long   0x1
+        .ascii "OpenBSD\0"
+        .long   0x0
+        .p2align 2
+
