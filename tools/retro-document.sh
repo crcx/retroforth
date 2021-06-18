139b322f792673757f8b4f46e9825a6fc60c2eb3
crc <crc@noemail.net>
2021-06-18 18:49:23 +0000

    retro-document: fix detection of sigils used
    FossilOrigin-Name: 22babfb155716d1834a20d3356ee8f94dd73430386f0a6296209cc2da85fce91

--- tools/retro-document.sh
+++ tools/retro-document.sh
@@ -16,4 +16,4 @@
 # ./describe sourcefile
 
 retro-unu $1 | tr " " "\n" | sed '/^[[:space:]]*$/d' | grep -Ev "^[\\\^\.\"\@\!\$\`:&'#$.\(\|]" | sort | uniq   | tr "\n" "\0" | xargs -0 retro-describe | cat -s
-retro-unu $1 | tr " " "\n" | sed '/^[[:space:]]*$/d' | cut -c1-1 | sort | uniq   | grep "[\\\^\.\"\@\!\$\`:&'#$.\(\|]" | sed 's/^/prefix:/'  | tr "\n" "\0" | xargs -0 retro-describe | cat -s
+retro-unu $1 | tr " " "\n" | sed '/^[[:space:]]*$/d' | cut -c1-1 | sort | uniq   | grep "[\\\^\.\"\@\!\$\`:&'#$.\(\|]" | sed 's/^/sigil:/'  | tr "\n" "\0" | xargs -0 retro-describe | cat -s
