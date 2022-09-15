5955634d14754a8b72caaaeec28c0a450772628b
crc <>
2022-09-15 12:56:12 +0000

    blocks device now working, enabled by default
    FossilOrigin-Name: c9ac0e5c8be62c047d1bacb8160cc3bb9203a6abf051a26b247c667e87db8786

--- vm/nga-c/retro.c
+++ vm/nga-c/retro.c
@@ -320,27 +320,43 @@ void io_malloc(NgaState *vm) {
 /* Block Storage -------------------------------------------- */
 #ifdef ENABLE_BLOCKS
 void io_blocks(NgaState *vm) {
-  CELL op = stack_pop(vm);
-  CELL buffer = stack_pop(vm);
-  CELL block = stack_pop(vm);
+  CELL op, buffer, block;
+  int32_t m[1024];
+  op = stack_pop(vm);
 
   if (op == 0) {
+    buffer = stack_pop(vm);
+    block = stack_pop(vm);
     int fp = open(vm->BlockFile, O_RDONLY, 0666);
     lseek(fp, 4096 * block, SEEK_SET);
-    read(fp, vm->memory + buffer, 4096);
+    read(fp, m, 4096);
+    for (int i = 0; i < 1024; i++) {
+      vm->memory[buffer + i] = (CELL)m[i];
+    }
     close(fp);
   }
 
   if (op == 1) {
+    buffer = stack_pop(vm);
+    block = stack_pop(vm);
     int fp = open(vm->BlockFile, O_WRONLY, 0666);
     lseek(fp, 4096 * block, SEEK_SET);
-    write(fp, vm->memory + buffer, 4096);
+    for (int i = 0; i < 1024; i++) {
+      m[i] = (int32_t)vm->memory[buffer + i];
+    }
+    write(fp, m, 4096);
     close(fp);
   }
 
+  if (op == 2) {
+    buffer = stack_pop(vm);
+    strlcpy(vm->BlockFile, string_extract(vm, buffer), 1024);
+  }
 }
 
-void query_blocks(NgaState *) {
+void query_blocks(NgaState *vm) {
+  stack_push(vm, 0);
+  stack_push(vm, 3);
 }
 #endif
 
@@ -1934,6 +1950,9 @@ int main(int argc, char **argv) {
 #ifdef BIT64
   register_device(vm, io_malloc, query_malloc);
 #endif
+#endif
+#ifdef ENABLE_BLOCKS
+  register_device(vm, io_blocks, query_blocks);
 #endif
 #ifdef ENABLE_CLOCK
   register_device(vm, io_clock, query_clock);
