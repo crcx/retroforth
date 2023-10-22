fd50adfb70ae2c3973fb6e155d2f02569cec782d
crc <>
2023-10-22 15:56:22 +0000

    remove unused file; update tools/missing-dsource.retro; dev. builds are set to 32-bit for now (will be back to 64-bit default later)
    FossilOrigin-Name: ef89aa704a826e2edd4ba3441105cb6f0bcc02a9b8b8f7f431a44e4d09cc0510

--- build-wip.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo cc -DFAST -DENABLE_FLOATS -DENABLE_FILES -DENABLE_UNIX -DENABLE_RNG -DENABLE_CLOCK -DENABLE_SCRIPTING -O2 -pipe -o ../../bin/retro-wip retro-wip.c -lm
-cd vm/nga-c && cc -DFAST -DENABLE_FLOATS -DENABLE_FILES -DENABLE_UNIX -DENABLE_RNG -DENABLE_CLOCK -DENABLE_SCRIPTING -O2 -pipe -o ../../bin/retro-wip retro-wip.c -lm
