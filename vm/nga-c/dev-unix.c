477d249b51f3ae920055ec9971d5124ca7053e76
crc <>
2023-12-09 18:31:45 +0000

    nga-c: formatting cleanups in dev-unix.c
    FossilOrigin-Name: 5fe417f0ca0f346e5c4d31a33884387d7f1703083b9b8545b715f6ff93767e1b

--- vm/nga-c/dev-unix.c
+++ vm/nga-c/dev-unix.c
@@ -16,9 +16,9 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-/*---------------------------------------------------------------------
-  `unix_open_pipe()` is like `file_open()`, but for pipes. This pulls
-  from the data stack:
+/*--------------------------------------------------------------
+  `unix_open_pipe()` is like `file_open()`, but for pipes. This
+  pulls from the data stack:
 
   - mode       (number, TOS)
   - executable (string, NOS)
@@ -31,15 +31,15 @@
   |  1   | w              | Open for writing     |
   |  3   | r+             | Open for read/update |
 
-  The file name should be a NULL terminated string. This will attempt
-  to open the requested file and will return a handle (index number
-  into the `OpenFileHandles` array).
+  The file name should be a NULL terminated string. This will
+  attempt to open the requested file and will return a handle
+  (index number into the `OpenFileHandles` array).
 
-  Once opened, you can use the standard file words to read/write to the
-  process.
-  ---------------------------------------------------------------------*/
+  Once opened, you can use the standard file words to read/write
+  to the process.
+  ------------------------------------------------------------*/
 
-void unix_open_pipe(NgaState *vm) {
+V unix_open_pipe(NgaState *vm) {
   CELL slot, mode, name;
   char *request;
   const char *modes[] = {"r", "w", "", "r+"};
@@ -57,22 +57,22 @@ void unix_open_pipe(NgaState *vm) {
   stack_push(vm, slot);
 }
 
-void unix_close_pipe(NgaState *vm) {
+V unix_close_pipe(NgaState *vm) {
   pclose(vm->OpenFileHandles[TOS]);
   vm->OpenFileHandles[TOS] = 0;
   stack_pop(vm);
 }
 
-void unix_system(NgaState *vm) {
+V unix_system(NgaState *vm) {
   int ignore = 0;
   ignore = system(string_extract(vm, stack_pop(vm)));
 }
 
-void unix_fork(NgaState *vm) {
+V unix_fork(NgaState *vm) {
   stack_push(vm, fork());
 }
 
-void unix_run_external(NgaState *vm) {
+V unix_run_external(NgaState *vm) {
   char *line, *args[128];
   int i, status;
   pid_t pid;
@@ -108,24 +108,24 @@ void unix_run_external(NgaState *vm) {
 }
 
 
-/*---------------------------------------------------------------------
-  UNIX provides `execl` to execute a file, with various forms for
-  arguments provided.
+/*--------------------------------------------------------------
+  UNIX provides `execl` to execute a file, with various forms
+  for arguments provided.
 
-  RRE wraps this in several functions, one for each number of passed
-  arguments. See the Glossary for details on what each takes from the
-  stack. Each of these will return the error code if the execution
-  fails.
-  ---------------------------------------------------------------------*/
+  Retro wraps this in several functions, one for each number of
+  passed arguments. See the Glossary for details on what each
+  takes from the stack. Each of these will return the error code
+  if the execution fails.
+  ------------------------------------------------------------*/
 
-void unix_exec0(NgaState *vm) {
+V unix_exec0(NgaState *vm) {
   char path[1025];
   strlcpy(path, string_extract(vm, stack_pop(vm)), 1024);
   execl(path, path, (char *)0);
   stack_push(vm, errno);
 }
 
-void unix_exec1(NgaState *vm) {
+V unix_exec1(NgaState *vm) {
   char path[1025];
   char arg0[1025];
   strlcpy(arg0, string_extract(vm, stack_pop(vm)), 1024);
@@ -134,7 +134,7 @@ void unix_exec1(NgaState *vm) {
   stack_push(vm, errno);
 }
 
-void unix_exec2(NgaState *vm) {
+V unix_exec2(NgaState *vm) {
   char path[1025];
   char arg0[1025], arg1[1025];
   strlcpy(arg1, string_extract(vm, stack_pop(vm)), 1024);
@@ -144,7 +144,7 @@ void unix_exec2(NgaState *vm) {
   stack_push(vm, errno);
 }
 
-void unix_exec3(NgaState *vm) {
+V unix_exec3(NgaState *vm) {
   char path[1025];
   char arg0[1025], arg1[1025], arg2[1025];
   strlcpy(arg2, string_extract(vm, stack_pop(vm)), 1024);
@@ -155,53 +155,38 @@ void unix_exec3(NgaState *vm) {
   stack_push(vm, errno);
 }
 
-void unix_exit(NgaState *vm) {
-  exit(stack_pop(vm));
-}
+V unix_exit(NgaState *vm) { exit(stack_pop(vm)); }
+V unix_getpid(NgaState *vm) { stack_push(vm, getpid()); }
+V unix_wait(NgaState *vm) { int a; stack_push(vm, wait(&a)); }
 
-void unix_getpid(NgaState *vm) {
-  stack_push(vm, getpid());
-}
-
-void unix_wait(NgaState *vm) {
-  int a;
-  stack_push(vm, wait(&a));
-}
-
-void unix_kill(NgaState *vm) {
-  CELL a;
-  a = stack_pop(vm);
+V unix_kill(NgaState *vm) {
+  CELL a = stack_pop(vm);
   kill(stack_pop(vm), a);
 }
 
-void unix_write(NgaState *vm) {
-  CELL a, b, c;
+V unix_write(NgaState *vm) {
   ssize_t ignore;
-  c = stack_pop(vm);
-  b = stack_pop(vm);
-  a = stack_pop(vm);
+  CELL c = stack_pop(vm);
+  CELL b = stack_pop(vm);
+  CELL a = stack_pop(vm);
   ignore = write(fileno(vm->OpenFileHandles[c]), string_extract(vm, a), b);
 }
 
-void unix_chdir(NgaState *vm) {
-  int ignore;
-  ignore = chdir(string_extract(vm, stack_pop(vm)));
+V unix_chdir(NgaState *vm) {
+  int ignore = chdir(string_extract(vm, stack_pop(vm)));
 }
 
-void unix_getenv(NgaState *vm) {
-  CELL a, b;
-  a = stack_pop(vm);
-  b = stack_pop(vm);
+V unix_getenv(NgaState *vm) {
+  CELL a = stack_pop(vm);
+  CELL b = stack_pop(vm);
   string_inject(vm, getenv(string_extract(vm, b)), a);
 }
 
-void unix_putenv(NgaState *vm) {
+V unix_putenv(NgaState *vm) {
   putenv(string_extract(vm, stack_pop(vm)));
 }
 
-void unix_sleep(NgaState *vm) {
-  sleep(stack_pop(vm));
-}
+V unix_sleep(NgaState *vm) { sleep(stack_pop(vm)); }
 
 Handler UnixActions[] = {
   unix_system,    unix_fork,   unix_exec0,     unix_exec1,
@@ -211,12 +196,12 @@ Handler UnixActions[] = {
   unix_sleep,     unix_run_external
 };
 
-void query_unix(NgaState *vm) {
+V query_unix(NgaState *vm) {
   stack_push(vm, 3);
   stack_push(vm, DEVICE_UNIX);
 }
 
-void io_unix(NgaState *vm) {
+V io_unix(NgaState *vm) {
   UnixActions[stack_pop(vm)](vm);
 }
 #endif
